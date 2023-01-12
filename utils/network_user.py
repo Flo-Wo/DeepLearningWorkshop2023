85b533b83f078bba9abae8fc1603ddf648f40f25
Flo-Wo <florianwolf1710@t-online.de>
2023-01-12 18:49:19 +0100

    add pass statements

--- utils/network_user.py
+++ utils/network_user.py
@@ -11,113 +11,11 @@ from utils.plotting import CacheLoss, plot_performance
 
 
 class NetworkUser:
-    def __init__(
-        self,
-        network_class: nn.Module,
-        loss_function: nn.Module,
-        data_loader: torch.utils.data.DataLoader,
-    ):
-        """Decouples the train and test process. Load parameters and evaluate the model.
+    def __init__(self):
+        pass
 
-        Parameters
-        ----------
-        network_class : nn.Module
-            Class of the network, e.g. ``DNN``.
-        loss_function : nn.Module
-            Loss function, e.g. ``torch.nn.CrossEntropyLoss``.
-        data_loader : torch.utils.data.DataLoader
-            Data loader to provide the test dataset via:
-            ```python
-            test_data = data_loader('test')
-            ```
-        """
-        self.network_class = network_class
-        self.loss_function = loss_function
-        self.data_loader = data_loader
-        self.path_to_network = None
-        self.folder = "./metadata"
-
-    def test_network(
-        self,
-        network_params: dict,
-        loss_params: dict,
-        path_to_network: str,
-        plot_loss: bool = True,
-        folder: str = "./metadata/",
-    ):
-        """Evaluate the model on the test dataset.
-
-        Parameters
-        ----------
-        network_params : dict
-            Parameters of the network, for a detailed description see ``network_params``
-           in the ``NetworkTrainer``-class.
-        loss_params : dict
-            Parameters used for the loss function.
-        path_to_network : str
-            String of the previously trained network, given by the ``NetworkTrainer``, e.g. by
-            ```python
-            >>> path = trainer.train_network(
-            >>>     12,
-            >>>     network_params,
-            >>>     loss_params,
-            >>>     {"lr": 0.001},
-            >>>     scheduler_params=dict(factor=0.9, patience=1),
-            >>>     )
-            >>> user = NetworkUser(DNN, torch.nn.CrossEntropyLoss, data_loader)
-            >>> user.test_network(network_params, loss_params, path)
-            ```
-            you can load the network automatically.
-        plot_loss : bool
-            Plot the train and validation loss curves, by default True.
-        folder : str, optional
-            Folder to store meta information, by default "./metadata/".
-        Returns
-        -------
-        CacheLoss
-            Helper class to save the test loss, metadata and the label. This will simplify
-            the plotting procedure if we want to compare multiple networks.
-        """
-        self.path_to_network = path_to_network
-        self.folder = folder
-
-        self.metadata = self._load_metadata()
-
-        model = self._load_network(network_params, self.path_to_network, self.folder)
-        criterion = self.loss_function(**loss_params)
-
-        test_dataset = self.data_loader("test")
-        len_test_data = len(test_dataset.dataset)
-
-        test_loss = 0
-        test_correct = 0
-        # turn off backpropagation for more efficiency
-        with torch.no_grad():
-            for batch_idx, (*input_data, target_output) in enumerate(
-                tqdm(test_dataset, "Test")
-            ):
-                predicted_output = model(*input_data)
-                loss = criterion(predicted_output, target_output)
-                test_loss += loss.item()
-
-                pred = predicted_output.argmax(
-                    dim=1, keepdim=True
-                )  # get the index of the max log-probability
-                test_correct += pred.eq(target_output.view_as(pred)).sum().item()
-
-        test_loss /= len_test_data
-        print(
-            "Train set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)".format(
-                test_loss,
-                test_correct,
-                len_test_data,
-                100.0 * test_correct / len_test_data,
-            )
-        )
-        cache_loss = CacheLoss(self.metadata, test_loss, label=str(self.network_class))
-        if plot_loss:
-            plot_performance(cache_loss)
-        return cache_loss
+    def test_network(self):
+        pass
 
     def get_predictor(
         self,
